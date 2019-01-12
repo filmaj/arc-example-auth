f5bc9728463ad3edfa8ed95e15d549b3c1aa8ce4
filmaj <maj.fil@gmail.com>
2019-01-12 09:08:49 -0600

    🐛✨better error handlign in signup, also check for email first before creating an account, and check that email is not empty

--- src/http/post-signup/index.js
+++ src/http/post-signup/index.js
@@ -5,18 +5,56 @@ let hash = require('bcryptjs').hashSync;
 let salt_rounds = 12;
 
 exports.handler = async function create_account (req) {
-    console.log(req);
     let location = url('/');
-    let session = {};
+    let session = await arc.http.session.read(req);
+    let email = req.body.email;
+    if (!email || !email.length) {
+        return {
+            status: 400,
+            type: 'application/json',
+            body: JSON.stringify({error: 'no email provided'})
+        };
+    }
+    // test if account already exists
+    try {
+        let account = await data.accounts.get({accountID: email});
+        if (account && account.accountID === email) {
+            return {
+                status: 400,
+                type: 'application/json',
+                body: JSON.stringify({error: 'email already registered'})
+            };
+        }
+    } catch (e) {
+        return {
+            status: 500,
+            type: 'application/json',
+            body: JSON.stringify(e)
+        };
+    }
+    // create the account
     try {
         let password = req.body.password;
         let hashed = hash(password, salt_rounds);
-        let account = {accountID: req.body.email, hash: hashed};
-        session.account = account;
+        let account = {accountID: email, hash: hashed};
         let result = await data.accounts.put(account);
-        console.log(result);
+        if (result && result.accountID) {
+            delete result.hash;
+            session.account = account;
+            console.log('session', session);
+        } else {
+            return {
+                status: 500,
+                type: 'application/json',
+                body: JSON.stringify({err: 'error during account creation, got unexpected result from db', result})
+            };
+        }
     } catch (e) {
-        console.error(e);
+        return {
+            status: 500,
+            type: 'application/json',
+            body: JSON.stringify(e)
+        };
     }
     return {
         status: 302,
