c9c4765c0145e0e6195fd5ec8a333190a907480d
filmaj <maj.fil@gmail.com>
2019-01-13 22:43:33 -0600

    resilient delete

--- src/http/post-notes-000noteID-del/index.js
+++ src/http/post-notes-000noteID-del/index.js
@@ -1,18 +1,28 @@
 let arc = require('@architect/functions');
 let data = require('@architect/data');
+let auth = require('@architect/shared/middleware/auth');
 let url = arc.http.helpers.url;
 
-exports.handler = async function route (req) {
+async function route (req) {
     let noteID = req.params.noteID;
     let session = await arc.http.session.read(req);
     let accountID = session.account.accountID;
-    // TODO: what happens if this fails?
-    await data.notes.delete({
-        noteID, accountID
-    });
+    try {
+        await data.notes.delete({
+            noteID, accountID
+        });
+    } catch (e) {
+        return {
+            status: 500,
+            type: 'application/json',
+            body: JSON.stringify(e)
+        };
+    }
     return {
         status: 302,
         cookie: await arc.http.session.write(session),
         location: url('/')
     };
-};
+}
+
+exports.handler = arc.middleware(auth, route);
